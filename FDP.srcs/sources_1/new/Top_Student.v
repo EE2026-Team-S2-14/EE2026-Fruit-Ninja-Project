f3a6fe4d31f3cafe35de052daed41ae2c3d8f51a
Atulteja <atulteja@hotmail.com>
2025-04-02 00:25:33 +0800

    added staggering launch of the fruits

--- FDP.srcs/sources_1/new/Top_Student.v
+++ FDP.srcs/sources_1/new/Top_Student.v
@@ -1,3 +1,4 @@
+
 `timescale 1ns / 1ps
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -35,6 +36,13 @@ module Top_Student(input basys_clock, input btnC, inout PS2Clk, inout PS2Data, o
     assign x_coordinate = pixel_index % 96;
     assign y_coordinate = pixel_index / 96;
     
+    wire [7:0] random_num;
+    reg [7:0] random_nums[0:9];
+    integer i;
+    
+    
+    random_number_generator r1 (.basys_clock(basys_clock), .out(random_num));
+    
     flexible_clock_divider unit_45Hz_clock(.basys_clock(basys_clock), .count_up_to(m_45Hz), .new_clock(clk_45Hz));
     flexible_clock_divider unit_6p25MHz_clock(.basys_clock(basys_clock), .count_up_to(m_6p25MHz), .new_clock(clk_6p25MHz));
     flexible_clock_divider unit_15Hz_clock(.basys_clock(basys_clock), .count_up_to(m_15Hz), .new_clock(clk_15Hz));
@@ -48,33 +56,108 @@ module Top_Student(input basys_clock, input btnC, inout PS2Clk, inout PS2Data, o
     );
     
     //wire btn;
-    wire [15:0] oled_fruit;
+    wire [15:0] oled_fruit[0:9];
     wire [15:0] oled_mouse;
-    reg collision_flag = 0;
+    reg collision_flag[0:9];
     reg [6:0] count = 0;
     assign led = count;
-   
+    reg [5:0] delay_flag = 0;
+    reg [15:0] delay_array[0:9];
+    
+    initial begin
+        delay_array[0] = 100;
+        delay_array[1] = 200;
+        delay_array[2] = 300;
+        delay_array[3] = 400;
+        delay_array[4] = 500;
+        delay_array[5] = 600;
+        delay_array[6] = 700;
+        delay_array[7] = 800;
+        delay_array[8] = 900;
+        delay_array[9] = 1000;
+    end
+
+    
+    always @(posedge clk_15Hz) begin
+            if (btnC) begin
+                random_nums[0] <= random_num;
+                delay_flag <= 1;
+            end else if (delay_flag == 1) begin
+                random_nums[1] <= random_num;
+                delay_flag <= 2;
+            end else if (delay_flag == 2) begin
+                random_nums[2] <= random_num;
+                delay_flag <= 3;
+            end else if (delay_flag == 3) begin
+                random_nums[3] <= random_num;
+                delay_flag <= 4;
+            end else if (delay_flag == 4) begin
+                random_nums[4] <= random_num;
+                delay_flag <= 5;
+            end else if (delay_flag == 5) begin
+                random_nums[5] <= random_num;
+                delay_flag <= 6;
+            end else if (delay_flag == 6) begin
+                random_nums[6] <= random_num;
+                delay_flag <= 7;
+            end else if (delay_flag == 7) begin
+                random_nums[7] <= random_num;
+                delay_flag <= 8;
+            end else if (delay_flag == 8) begin
+                random_nums[8] <= random_num;
+                delay_flag <= 9;
+            end else if (delay_flag == 9) begin
+                random_nums[9] <= random_num;
+                delay_flag <= 0;
+            end
+        end
+
+    
     //debounce_c debouncer (.clk_1kHz(clk_1KHz), .btnC(btn));
     
-    parabolic_motion unit_1(.basys_clock(basys_clock), .collision_flag(collision_flag), .clk_15Hz(clk_15Hz), .btnC(btnC), .x_coordinate(x_coordinate), 
-    .y_coordinate(y_coordinate), .oled_data(oled_fruit));
+    generate
+        genvar idx;
+        for (idx = 0; idx < 10; idx = idx + 1) begin : fruit_gen
+            parabolic_motion fruit (
+                .basys_clock(basys_clock),
+                .delay(delay_array[idx]),
+                .collision_flag(collision_flag[idx]),
+                .clk_15Hz(clk_15Hz),
+                .x_coordinate(x_coordinate),
+                .y_coordinate(y_coordinate),
+                .random_num(random_nums[idx]),
+                .oled_data(oled_fruit[idx])
+            );
+        end
+    endgenerate
+
+        
     Mouse mouse (.basys_clock(basys_clock), .x_coordinate(x_coordinate), .y_coordinate(y_coordinate), 
     .PS2Clk(PS2Clk), .PS2Data(PS2Data), .oled_colour(oled_mouse));
             
     always @(posedge clk_25MHz) begin
-        if ((oled_mouse == 16'hF800) && oled_fruit == 16'b11111_111000_00000) begin
-            collision_flag <= (count == 30) ? 1 : 0;
-            count <= count + 1; 
+        for (i = 0; i < 10; i = i + 1) begin
+        if ((oled_mouse == 16'hF800) && (oled_fruit[i] == 16'b11111_111000_00000)) begin
+            collision_flag[i] <= (count == 30) ? 1 : 0;
+            count <= count + 1;
         end
+    end
+
         if (oled_mouse == 16'hF800 || oled_mouse == 16'h0000) begin
-            oled_colour <= oled_mouse; 
-        end else if (oled_fruit == 16'b11111_111000_00000) begin
-            oled_colour <= oled_fruit;
+            oled_colour <= oled_mouse;
         end else begin
             oled_colour <= 16'hFFFF;
+            for (i = 0; i < 10; i = i + 1) begin
+                if (oled_fruit[i] == 16'b11111_111000_00000) begin
+                    oled_colour <= oled_fruit[i];
+                end
+            end
         end
-        if (btnC) begin
-            collision_flag <= 0; 
+
+       if (btnC) begin
+            for (i = 0; i < 10; i = i + 1) begin
+                collision_flag[i] <= 0;
+            end
             count <= 0;
         end
     end
--- FDP.srcs/sources_1/new/parabolic_motion.v
+++ FDP.srcs/sources_1/new/parabolic_motion.v
@@ -21,11 +21,16 @@
 
 
 
-module parabolic_motion( input basys_clock, input collision_flag, input clk_15Hz, input btnC, input [6:0] x_coordinate, input [6:0] y_coordinate, 
-output reg [15:0] oled_data);
-
-    wire [7:0] rand_num;
-    random_number_generator r1 (.basys_clock(basys_clock), .out(rand_num));
+module parabolic_motion(
+    input basys_clock, 
+    input [15:0] delay, 
+    input collision_flag, 
+    input clk_15Hz, 
+    input [6:0] x_coordinate, 
+    input [6:0] y_coordinate, 
+    input [7:0] random_num, 
+    output reg [15:0] oled_data
+);
 
     reg [6:0] x = 10; 
     reg [6:0] y = 63; 
@@ -34,32 +39,44 @@ output reg [15:0] oled_data);
     reg signed [6:0] vy = 10;  // Initial vertical velocity
     reg signed [6:0] g = 1;   // Gravity
     reg [6:0] random_x_position = 10;
+    
+    reg [15:0] delay_count = 0;
+    reg active = 0;
 
     always @(posedge clk_15Hz) begin
-        if (btnC) begin
-            random_x_position <= (rand_num % 56) + 20;
-            vx <= (rand_num % 4) - 2;
-            y <= 64;
-            t <= 0;
-        end 
-        else if (y > 0 && y < 65) begin
-            t <= t + 1; 
-            x <= random_x_position + (vx * t);
-            y <= 64 - ((vy * t) - ((g * t * t) >> 1));
+        if (!active) begin
+            // Waiting period
+            if (delay_count < delay) begin
+                delay_count <= delay_count + 1;
+            end else begin
+                // Launch fruit
+                random_x_position <= (random_num % 56) + 20;
+                vx <= (random_num % 4) - 2;
+                y <= 63;
+                t <= 0;
+                active <= 1;
+            end
+        end else if (active) begin
+            if (y > 0 && y < 65 && !collision_flag) begin
+                t <= t + 1;
+                x <= random_x_position + (vx * t);
+                y <= 63 - ((vy * t) - ((g * t * t) >> 1));
+            end else begin
+                // Reset and wait for next launch
+                active <= 0;
+                delay_count <= 0;
+            end
         end
     end
 
     always @(*) begin
-//        if (collision_flag == 1) begin
-//            oled_data <= 16'b00000_000000_00000;
-//        end else
-        if (x_coordinate >= x && x_coordinate < x + 15 &&
-            y_coordinate >= y && y_coordinate < y + 15 && (collision_flag != 1)) begin
+        if (x_coordinate >= x && x_coordinate < x + 3 &&
+            y_coordinate >= y && y_coordinate < y + 3 && (collision_flag != 1)) begin
             oled_data = 16'b11111_111000_00000; 
-        end 
-        else begin
+        end else begin
             oled_data = 16'b00000_000000_00000;
         end
     end
 
 endmodule
+
